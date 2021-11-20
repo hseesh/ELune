4fedc2b0753a14373793e199d8f705e01bcc8c1f
hse <36833172+hseesh@users.noreply.github.com>
2021-11-20 21:06:52 +0800

    finsh part two

---
+++ src/com/yatoufang/test/dao/Test.java
@@ -0,0 +1,25 @@
+package com.yatoufang.test.dao;
+
+import com.yatoufang.entity.Table;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.log.NullLogChute;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author GongHuang（hse）
+ * @since 2021/11/20 20:05
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
